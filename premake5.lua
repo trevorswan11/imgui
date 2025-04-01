eee379c71ca1ced6d91ee4dc0130fdfd89ee1da4
Trevor Swan <tcs94@case.edu>
2025-04-01 19:40:02 -0400

    updated premake to abide by non-static runtime

--- premake5.lua
+++ premake5.lua
@@ -22,7 +22,7 @@ project "ImGui"
 	filter "system:windows"
 		systemversion "latest"
 		cppdialect "C++17"
-		staticruntime "on"
+		staticruntime "off"
 	
 	filter "configurations:Debug"
 		runtime "Debug"
